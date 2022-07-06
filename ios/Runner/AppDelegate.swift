9d92ee02c4b5c1572bdb70c6f6d8f30f02023abe
Flores <elizaflores.elf@gmail.com>
2022-07-06 15:44:20 -0700

    fixed android & ios files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Constants
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("${googleMapsKey}")
+    GMSServices.provideAPIKey(Constants.MAP_KEY)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
