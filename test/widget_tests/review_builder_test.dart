0d79b7a7800d6e0c086ed1781c80e8e1b2138a0d
Flores <elizaflores.elf@gmail.com>
2022-07-15 16:26:25 -0700

    implemented review_builder_test; found that testing avatar widget is unecessary and will be done with goldens

---
+++ test/widget_tests/review_builder_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const String reviewText = 'Review Text Here';
+
+  Widget reviewWithAvatar = MaterialApp(
+    home: Scaffold(
+      body: ListView.builder(
+          itemCount: 3,
+          shrinkWrap: true,
+          //physics: const ClampingScrollPhysics(),
+          itemBuilder: (_, index) {
+            return ListTile(
+              title: Column(
+                children: [
+                  RatingBarIndicator(
+                    rating: 4.5,
+                    itemBuilder: (context, index) => Icon(
+                      Icons.star,
+                      color: Colors.amber.shade600,
+                    ),
+                    itemCount: 5,
+                    unratedColor: Colors.amber.withOpacity(0.5),
+                  ),
+                  const Text(
+                    reviewText,
+                  ),
+                  Row(
+                    children: const [
+                      Text(
+                        'User Name',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+    ),
+  );
+
+  testWidgets('3 Review Tiles', (tester) async {
+    final threeReviews = reviewWithAvatar;
+    await tester.pumpWidget(threeReviews);
+    await tester.tap(find.byType(ListView));
+    await tester.pumpAndSettle();
+    expect(find.byType(ListTile), findsWidgets);
+    expect(find.text(reviewText), findsNWidgets(3));
+  });
+}
